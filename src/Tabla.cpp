1e7d902066ebf37904ae10b641731a2a7ab5d072
Karo Gorosito <kaarogor@gmail.com>
2019-11-11 14:28:11 +0000

    Se completa: -obtenerColumna

--- src/Tabla.cpp
+++ src/Tabla.cpp
@@ -46,5 +46,5 @@ const linear_set<Valor>& Tabla::valoresClave(){
 }
 
 const linear_map<linear_set<Registro>::iterator, Valor>& Tabla::obtenerColumna(const NombreCampo& campo){
-    // COMPLETAR
+    return *_diccColumnas.at(valor);
 }
